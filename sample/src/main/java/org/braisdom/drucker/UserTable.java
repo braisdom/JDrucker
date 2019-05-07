7ec0c047b553aba9cb1cf74b8670c4f0132f49c3
Braisdom <yonghe.wang@joowing.com>
2019-05-07 17:11:16 +0800

    InitCommit

--- sample/src/main/java/org/braisdom/drucker/UserTable.java
+++ sample/src/main/java/org/braisdom/drucker/UserTable.java
@@ -3,7 +3,6 @@ package org.braisdom.drucker;
 import org.braisdom.drucker.annotation.Table;
 import org.braisdom.drucker.database.TableBehavior;
 
-@Table("users")
 public interface UserTable extends TableBehavior {
 
 }
